9965191eeeebf6cd500abe9f41017c1ff6247213
Daniel <tretoleondaniel@gmail.com>
2025-11-02 16:39:06 +0100

    Atributos y constructor de tamagotchi definidos

--- src/main/java/cuatrovientos/dam/php/Tamagotchis/Tamagotchi.java
+++ src/main/java/cuatrovientos/dam/php/Tamagotchis/Tamagotchi.java
@@ -1,5 +1,33 @@
 package cuatrovientos.dam.php.Tamagotchis;
 
-public class Tamagotchi {
+import java.util.Random;
+
+
+public class Tamagotchi extends Thread {
+
+    private final int id;
+    private boolean vivo = true;
+    private int suciedad = 0;
+    private int hambre = 0;
+    private final long tiempoNacimiento;
+    private final long tiempoVida = 300000;
+    private final long tiempoComer; 
+    private boolean ocupado = false;
+
+    public Tamagotchi(int id) {
+        Random rnd = new Random();
+        this.id = id;
+        this.tiempoComer = 3000 + rnd.nextInt(4000);
+        this.tiempoNacimiento = System.currentTimeMillis();
+    }
+
+    public boolean estaVivo() { return vivo; }
+    public boolean estaOcupado() {return ocupado;}
+
+    @Override
+    public void run() {
+        
+    }
+
 
 }
