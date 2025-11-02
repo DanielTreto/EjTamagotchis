feba0ccd3820c7e5aade311c908298f818d2237f
Daniel <tretoleondaniel@gmail.com>
2025-11-02 18:35:40 +0100

    Métodos comer, ver estado y jugar modificados

--- src/main/java/cuatrovientos/dam/php/Tamagotchis/Cuidador.java
+++ src/main/java/cuatrovientos/dam/php/Tamagotchis/Cuidador.java
@@ -2,7 +2,7 @@ package cuatrovientos.dam.php.Tamagotchis;
 
 
 public class Cuidador {
-	public static void main(String[] args) {
-		
-	}
+    public static void main(String[] args) {
+        
+    } 	
 }
--- src/main/java/cuatrovientos/dam/php/Tamagotchis/Tamagotchi.java
+++ src/main/java/cuatrovientos/dam/php/Tamagotchis/Tamagotchi.java
@@ -8,10 +8,12 @@ public class Tamagotchi extends Thread {
     private final int id;
     private boolean vivo = true;
     private int suciedad = 0;
-    private int hambre = 0;
     private final long tiempoNacimiento;
     private final long tiempoVida = 300000;
     private final long tiempoComer; 
+    private int contadorComer;
+    private int contadorJugar;
+    private boolean enBaño = false;
     private boolean ocupado = false;
 
     public Tamagotchi(int id) {
@@ -28,28 +30,21 @@ public class Tamagotchi extends Thread {
     public void run() {
         System.out.println("¡Tamagotchi " + id + " está vivo!");
         long ultimoEnsuciamiento = System.currentTimeMillis();
-        long ultimoHambriento = System.currentTimeMillis();
 
         try {
             while (vivo) {
                 long edad = System.currentTimeMillis() - tiempoNacimiento;
 
                 // 5 minutos de vida
-                if (edad >= tiempoVida && vivo) {
+                if (edad >= tiempoVida) {
                     morir();
                     break;
                 }
 
                 // cada 20 segundos, ensuciar
-                if (System.currentTimeMillis() - ultimoEnsuciamiento >= 20000 && vivo) {
+                if (System.currentTimeMillis() - ultimoEnsuciamiento >= 20000 && !enBaño) {
                     ensuciar();
                     ultimoEnsuciamiento = System.currentTimeMillis();
-                }
-                
-             // cada 10 segundos, más hambriento
-                if (System.currentTimeMillis() - ultimoHambriento >= 10000 && vivo) {
-                    hambriento();
-                    ultimoHambriento = System.currentTimeMillis();
                 }
 
                 Thread.sleep(1000);
@@ -69,88 +64,86 @@ public class Tamagotchi extends Thread {
             num2 = rnd.nextInt(9) + 1;
         } while (num1 + num2 >= 10);
 
-        System.out.println("Tamagotchi " + id + " quiere jugar.");
+        System.out.println("\nTamagotchi " + id + " quiere jugar.");
 
         while (vivo) {
-            System.out.print("¿Cuál es el resultado de " + num1 + " + " + num2 + "? ");
+            System.out.print("\n¿Cuál es el resultado de " + num1 + " + " + num2 + "? ");
             int resultado = sc.nextInt();
 
             if (resultado == num1 + num2) {
-                System.out.println("¡Enhorabuena, has acertado!");
+                System.out.println("\n¡Enhorabuena, has acertado!");
                 ocupado = false;
+                contadorJugar++;
                 break;
             } else {
-                System.out.println("Respuesta incorrecta, sigue jugando...");
+                System.out.println("\nRespuesta incorrecta, sigue jugando...");
             }
         }
     }
+    
+    private String estadoAnimo() {
+        if (contadorJugar == 0) {
+            return "Está apagado y triste. Hace mucho que no juegas con él...";
+        } else if (contadorJugar == 1) {
+            return "Se siente algo aburrido. Un rato de juego le animaría.";
+        } else {
+            return "¡Está feliz y lleno de energía! Le encanta pasar tiempo contigo.";
+        }
+    }
+
 
     private void ensuciar() {
         suciedad++;
         if (suciedad == 5) {
             System.out.println("\nTamagotchi " + id + " está empezando a estar muy sucio.");
         } else if (suciedad >= 10) {
-            System.out.println("💀 Tamagotchi " + id + " ha muerto de suciedad.");
+            System.out.println("\nTamagotchi " + id + " ha muerto de suciedad.");
             vivo = false;
         }
     }
 
     public void limpiar() {
     	ocupado = true;
+    	enBaño = true;
         if (!vivo) return;
         new Thread(() -> {
-            System.out.println("Tamagotchi " + id + " entra al baño (5s)...");
+            System.out.println("\nTamagotchi " + id + " entra al baño (5s)...");
             try { Thread.sleep(5000); } catch (InterruptedException ignored) {}
             suciedad = 0;
-            System.out.println("Tamagotchi " + id + " sale limpio y feliz 😊");
+            System.out.println("\nTamagotchi " + id + " sale limpio y feliz");
             ocupado = false;
+            enBaño = false;
         }).start();
-    }
-    
-    private void hambriento() {
-        hambre++;
-        if (hambre == 5) {
-            System.out.println("\nTamagotchi " + id + " está empezando a tener hambre.");
-        } else if (suciedad >= 10) {
-            System.out.println("💀 Tamagotchi " + id + " ha muerto de hambre.");
-            vivo = false;
-        }
     }
 
     public void comer() {
     	ocupado = true;
         if (!vivo) return;
         new Thread(() -> {
-            System.out.println("Tamagotchi " + id + " comienza a comer ("+tiempoComer+"ms)...");
+            System.out.println("\nTamagotchi " + id + " comienza a comer ("+tiempoComer+"ms)...");
             try {
                 Thread.sleep(tiempoComer);
             } catch (InterruptedException ignored) {}
-            System.out.println("Tamagotchi " + id + " ha terminado de comer.");
-            hambre=0;
+            System.out.println("\nTamagotchi " + id + " ha terminado de comer.");
+            contadorComer++;
             ocupado = false;
-
         }).start();
     }
 
     public void matar() {
-        if (!vivo) return;
-        if (ocupado) {
-        	System.out.println("No puedes matar al Tamagothi "+id+", esta ocupado");
-        	return;
-        }
-        System.out.println("Has matado a Tamagotchi " + id + " 💀");
+        System.out.println("\nHas matado a Tamagotchi " + id);
         vivo = false;
     }
 
     private void morir() {
-        System.out.println("Tamagotchi " + id + " ha muerto de vejez 😢");
+        System.out.println("\nTamagotchi " + id + " ha muerto de vejez");
         vivo = false;
     }
     
     public void verEstado() {
-    	System.out.println("Tamagotchi " + id);
-    	System.out.println("Hambre (0-10): "+ hambre);
+    	System.out.println("\nTamagotchi " + id);
+    	System.out.println("Nº de veces que ha comido: "+ contadorComer);
     	System.out.println("Suciedad (0-10): "+ suciedad);
-    	System.out.println("Aburrimiento (0-10): ");
+    	System.out.println("Estado de ánimo: "+ estadoAnimo());
     }
 }
